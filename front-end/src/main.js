9f7185db3c7039bdeed6786f430c05dca564a117
ZA2025 <zainabal2025@gmail.com>
2024-02-09 16:27:29 +0000

    some changes

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./main.css";
-import { createRouter, createWebHashHistory } from "vue-router";
+import * as VueRouter from "vue-router";
 import ShoppingCartPage from "./pages/ShoppingCartPage.vue";
 import ProductsPage from "./pages/ProductsPage.vue";
 import ProductDetailPage from "./pages/ProductDetailPage.vue";
@@ -9,10 +9,10 @@ import NotFoundPage from "./pages/NotFoundPage.vue";
 
 createApp(App)
   .use(
-    createRouter({
-      history: createWebHashHistory(),
+    VueRouter.createRouter({
+      history: VueRouter.createWebHistory(),
       routes: [
-        { path: "/", component: ProductsPage },
+        { path: "/products", component: ProductsPage },
         { path: "/cart", component: ShoppingCartPage },
         { path: "/products/:id", component: ProductDetailPage },
         { path: "/:pathMatch(.*)*", component: NotFoundPage },
