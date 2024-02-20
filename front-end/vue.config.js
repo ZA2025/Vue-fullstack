886e3bb8627c172fbe1749438eedf4909f951b97
ZA2025 <zainabal2025@gmail.com>
2024-02-20 21:55:29 +0000

    changes to db

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/products/'
-    : '/',
   transpileDependencies: true,
   devServer: {
     proxy: "http://localhost:8000",
