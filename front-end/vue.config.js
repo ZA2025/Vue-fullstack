dff2b4f90173bc5a962a8b60fae11807301e0ed0
ZA2025 <zainabal2025@gmail.com>
2024-02-20 20:19:32 +0000

    changes to vonfig file

--- front-end/vue.config.js
+++ front-end/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/products/'
+    : '/',
   transpileDependencies: true,
   devServer: {
     proxy: "http://localhost:8000",
